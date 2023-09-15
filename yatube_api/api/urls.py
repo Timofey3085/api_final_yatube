c1f2c9934adf29e38f96331256dc558a51dd1a2d
Тимофей Разборщиков <ash_tray85@mail.ru>
2023-09-15 22:21:53 +0300

    Change main.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowsViewSet
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
 
 v1_router = DefaultRouter()
 
@@ -14,4 +16,11 @@ urlpatterns = [
     path('v1/', include(v1_router.urls)),
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
+]
+
+jwt_patterns = [
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
-from posts.models import Post, Group, Follow
+from rest_framework import viewsets, mixins
+from posts.models import Post, Group, Follow, Comment, User
 from api.serializers import (CommentSerializer, GroupSerializer,
                              PostSerializer, FollowSerializer)
 from django.shortcuts import get_object_or_404
@@ -29,21 +29,24 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     """Вьюсет получения, записи и изменения комментариев."""
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthorOrReadOnly]
 
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
     def get_queryset(self):
         """Метод выбора всех комментариев по нужному посту."""
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        return post.comments.all()
+        return self.get_post().comments
 
     def perform_create(self, serializer):
         """Метод создания нового комментария по нужному посту."""
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
 
 
-class FollowsViewSet(viewsets.ModelViewSet):
+class FollowsViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
+                     viewsets.GenericViewSet):
     """Вьюсет для обработки подписок."""
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
@@ -52,7 +55,10 @@ class FollowsViewSet(viewsets.ModelViewSet):
     search_fields = ("following__username",)
 
     def get_queryset(self):
-        return Follow.objects.filter(user=self.request.user)
+
+        following = get_object_or_404(User,
+                                      username=self.request.user.username)
+        return following.follower
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
